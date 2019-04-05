33f251b62bbc12b339917f69f419ef466d3332bf
hulfyback <hulfyback@gmail.com>
2019-04-05 16:03:04 +0200

    added GuessMyNumbr and renamed CreatePalindrome

--- week-01/day-5/Java/Exercises/src/solutions/CreatePalindrome.java
+++ week-01/day-5/Java/Exercises/src/solutions/CreatePalindrome.java
@@ -1,6 +1,6 @@
 import solutions.Anagram;
 
-public class CreatePalindrom {
+public class CreatePalindrome {
   public static String createPalindrom(String input){
     String palindrome = input;
     palindrome += Anagram.reverseString(input);
--- week-01/day-5/Java/Exercises/src/solutions/GuessMyNumber.java
+++ week-01/day-5/Java/Exercises/src/solutions/GuessMyNumber.java
@@ -1,4 +1,45 @@
 package solutions;
 
+import java.util.Scanner;
+
 public class GuessMyNumber {
+  public static void guessMyNumber(){
+
+    int number = (int) (Math.random() * 100);
+    int guess;
+    int lives = 5;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("I've the number between 1-100. You have 5 lives.");
+    do {
+      System.out.println("Enter your guess: ");
+      guess = scanner.nextInt();
+      if(number > guess){
+        lives--;
+        if(lives == 0){
+          System.out.println("You lost!");
+          break;
+        }
+        System.out.println("Too low. You have " + lives + " lives.");
+      }
+      else if(number < guess){
+        lives--;
+        if(lives == 0){
+          System.out.println("You lost!");
+          break;
+        }
+        System.out.println("Too high. You have " + lives + " lives.");
+      }
+
+      else {
+        System.out.println("Congratulations. You won!");
+        break;
+      }
+    }
+    while(true);
+    if(lives == 0){
+      System.out.println("The number was: " + number);
+    }
+  }
 }
