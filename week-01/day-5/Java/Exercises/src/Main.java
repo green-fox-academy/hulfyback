2d8a7cafc55d52ef5aa036371ebcf8edc134e931
hulfyback <hulfyback@gmail.com>
2019-04-07 22:03:55 +0200

    modified Main.java

--- week-01/day-5/Java/Exercises/src/Main.java
+++ week-01/day-5/Java/Exercises/src/Main.java
@@ -2,11 +2,35 @@ import solutions.*;
 
 public class Main {
   public static void main(String[] args) {
-    DrawDiamond.drawDiamond();
-    DrawSquare.drawSquare();
-    DrawDiagonal.drawDiagonal();
-    GuessTheNumber.guessTheNumber();
-    ParametricAverage.parametricAerage();
-    DrawChessTable.drawChessTable();
+//    DrawDiamond.drawDiamond();
+//    DrawSquare.drawSquare();
+//    DrawDiagonal.drawDiagonal();
+//    GuessTheNumber.guessTheNumber();
+//    ParametricAverage.parametricAerage();
+//    DrawChessTable.drawChessTable();
+//    System.out.println(SubStr.subStr("this is what I'm searching in", "searching"));
+//    System.out.println(SubStr.subStr("this is what I'm searching in", "not"));
+//    System.out.println(Anagram.anagram("dog", "god"));
+
+//    CreatePalindrome.createPalindrom("greenfox");
+
+
+//    GuessMyNumber.guessMyNumber();
+//    ArmstrongNumber.printIfArmstrongNumber(1634);
+//    ArmstrongNumber.printIfArmstrongNumber(153);
+//    ArmstrongNumber.printIfArmstrongNumber(371);
+//    ArmstrongNumber.printIfArmstrongNumber(12);
+
+//    PalindromeSearcher.printPalindromesFromAString("dog goat dad duck doodle never"	);
+//    PalindromeSearcher.printPalindromesFromAString("apple"	);
+//    PalindromeSearcher.printPalindromesFromAString("racecar");
+
+    System.out.println(Josephus.getJosephusNumber(1));
+    System.out.println(Josephus.getJosephusNumber(2));
+    System.out.println(Josephus.getJosephusNumber(3));
+    System.out.println(Josephus.getJosephusNumber(7));
+    System.out.println(Josephus.getJosephusNumber(13));
+    System.out.println(Josephus.getJosephusNumber(12));
+    System.out.println(Josephus.getJosephusNumber(41));
   }
 }
