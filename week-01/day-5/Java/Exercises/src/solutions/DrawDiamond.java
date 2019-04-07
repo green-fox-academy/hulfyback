a3a4556ff0115d966ff4d5933ea347c247fde4dd
hulfyback <hulfyback@gmail.com>
2019-04-07 22:51:27 +0200

    made some changes in style

--- week-01/day-5/Java/Exercises/src/solutions/DrawDiamond.java
+++ week-01/day-5/Java/Exercises/src/solutions/DrawDiamond.java
@@ -18,29 +18,26 @@ public class DrawDiamond {
 //
 // The diamond should have as many lines as the number was
 
-  public static void  drawDiamond(){
+  public static void printBodyOfDiamond(int n, int i){
+    for(int j = 0; j < n - i; j++){
+      System.out.print(' ');
+    }
+    for (int j = 0; j < i * 2 + -1; j++){
+      System.out.print('*');
+    }
+    System.out.println();
+  }
+
+  public static void drawDiamond() {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter the number of lines of the diamond: ");
     int numberOfLines = scanner.nextInt();
 
     for (int i = 1; i <= (int) Math.ceil((double)numberOfLines / 2) ; i++) {
-      for(int j = 0; j < numberOfLines - i; j++){
-        System.out.print(' ');
-      }
-      for (int j = 0; j < i * 2 + -1; j++){
-        System.out.print('*');
-      }
-      System.out.println();
-
+      printBodyOfDiamond(numberOfLines, i);
     }
-    for (int i = (int) Math.ceil(numberOfLines / 2); i >= 1; i--){
-      for(int j = 0; j < numberOfLines - i; j++){
-        System.out.print(' ');
-      }
-      for (int j = 0; j < i * 2 + -1; j++){
-        System.out.print('*');
-      }
-      System.out.println();
+    for (int i = (int) Math.ceil(numberOfLines / 2); i >= 1; i--) {
+      printBodyOfDiamond(numberOfLines, i);
     }
   }
 }
