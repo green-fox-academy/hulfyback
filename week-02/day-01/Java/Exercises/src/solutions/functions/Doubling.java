a2c2b39018082090f3b450dd8b11936faed573f1
hulfyback <hulfyback@gmail.com>
2019-04-08 14:05:05 +0200

    Added Doubling.java

---
+++ week-02/day-01/Java/Exercises/src/solutions/functions/Doubling.java
@@ -0,0 +1,11 @@
+package solutions.functions;
+
+public class Doubling {
+  // - Create an integer variable named `baseNum` and assign the value `123` to it
+  // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+  // - Print the result of `doubling(baseNum)`
+
+  public static int doubling(int input) {
+    return input * 2;
+  }
+}
