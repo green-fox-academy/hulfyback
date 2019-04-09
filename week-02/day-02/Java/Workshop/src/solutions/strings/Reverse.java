449d1e88cb152d656123eaff01a1ce43332bfde2
hulfyback <hulfyback@gmail.com>
2019-04-09 13:07:33 +0200

    Added Reverse.java

---
+++ week-02/day-02/Java/Workshop/src/solutions/strings/Reverse.java
@@ -0,0 +1,24 @@
+package solutions.strings;
+
+public class Reverse {
+
+  static String reverse(String input) {
+    String output = "";
+
+    for (int i = input.length() - 1; i >= 0; i--) {
+      output = output + (input.charAt(i));
+    }
+    return output;
+  }
+
+  public static void main(String... args){
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a method that can reverse a String, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
+
+
+    System.out.println(reverse(reversed));
+  }
+}
