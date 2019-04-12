35136ba6a00dfea9dcef685e2e39e8098538dd13
hulfyback <hulfyback@gmail.com>
2019-04-12 11:27:34 +0200

    feat(purple-steps-3d): recursion steps

--- week-02/day-03/Java/Workshop/src/solutions/PurpleSteps3d.java
+++ week-02/day-03/Java/Workshop/src/solutions/PurpleSteps3d.java
@@ -3,28 +3,23 @@ package solutions;
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.Scanner;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps3d {
-  public static void mainDraw(Graphics graphics) {
-    // Reproduce this:
-    // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
-    graphics.setColor(new Color(128, 0, 128));
-    int x = 10;
-    int y = 10;
-    for (int i = 10; i < 70 ; i += 10) {
-      graphics.fill3DRect(x, y, i, i, true);
-      x += i;
-      y += i;
-    }
-  }
-
-  // Don't touch the code below
-  static int WIDTH = 320;
-  static int HEIGHT = 320;
+  private static final int WIDTH = 320;
+  private static final int HEIGHT = 320;
+  private static final int X_COORD = 10;
+  private static final int Y_COORD = 10;
+  private static final int SIZE = 10;
+  private static final int MAXSIZE = 60;
 
   public static void main(String[] args) {
+    createFrame();
+  }
+
+  public static void createFrame(){
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     ImagePanel panel = new ImagePanel();
@@ -35,11 +30,31 @@ public class PurpleSteps3d {
     jFrame.pack();
   }
 
+  public static void drawPurpleSteps(int x, int y, int size, Graphics graphics) {
+    graphics.setColor(new Color(128, 0, 128));
+
+    for (int i = SIZE; i < MAXSIZE ; i += SIZE) {
+    graphics.fill3DRect(x, y, i, i, true);
+    x += i;
+    y += i;
+    }
+  }
+
+  public static void drawStepsRecursively(int x, int y, int size, Graphics graphics) {
+    graphics.setColor(new Color(128, 0, 128));
+
+    graphics.fill3DRect(x, y, size, size, true);
+    if (size < MAXSIZE) {
+      drawStepsRecursively(x + size, y + size, size + 10, graphics);
+    }
+  }
+
   static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
-      mainDraw(graphics);
+      drawPurpleSteps(X_COORD, Y_COORD, SIZE, graphics);
+      drawStepsRecursively(X_COORD, Y_COORD, SIZE, graphics);
     }
   }
 }
