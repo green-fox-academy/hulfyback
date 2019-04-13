7e3c99dfbfd8f4c752203c821bd29068a8f31133
hulfyback <hulfyback@gmail.com>
2019-04-13 18:04:45 +0200

    feat(recursive-drawing): add new main class

---
+++ week-02/day-05/Java/Project/src/MainDraw.java
@@ -0,0 +1,6 @@
+public class MainDraw {
+  public static void main(String[] args) {
+//    DrawingFrame.createFrame(320, 320, "Envelpe Star", new EnvelopeStar());
+    DrawingFrame.createFrame(320, 320, "Lines", new Lines());
+  }
+}
