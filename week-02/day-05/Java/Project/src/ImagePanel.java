07b646ec5e545e8e27882aebbba014b1c34c7352
hulfyback <hulfyback@gmail.com>
2019-04-13 18:04:20 +0200

    feat(recursive-drawing): create new ImagePanel

---
+++ week-02/day-05/Java/Project/src/ImagePanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel {
+  @Override
+  protected void paintComponent(Graphics graphics) {
+    super.paintComponent(graphics);
+  }
+}
