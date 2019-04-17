56d5f14bd98cb08625a718a1bcbf426b622bf1f9
hulfyback <hulfyback@gmail.com>
2019-04-17 16:47:11 +0200

    feat(OOP-project): add sharpieset

--- week-03/day-03/OOP/src/SharpieSet.java
+++ week-03/day-03/OOP/src/SharpieSet.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SharpieSet {
+  int countOfUsableSharpies = 0;
+  private List<Sharpie> sharpieList = new ArrayList<>();
+
+  public List<Sharpie> getSharpieList() {
+    return sharpieList;
+  }
+
+  public void setSharpieList(List<Sharpie> sharpieList) {
+    this.sharpieList = sharpieList;
+  }
+
+  public void addSharpie(Sharpie sharpie) {
+    this.getSharpieList().add(sharpie);
+  }
+
+  public int countUsable() {
+    this.countOfUsableSharpies = 0;
+    for (Sharpie sharpie : this.getSharpieList()) {
+      if (sharpie.getInkAmount() != 0.0f) {
+        this.countOfUsableSharpies++;
+      }
+    }
+    return countOfUsableSharpies;
+  }
+
+  public void removeTrash() {
+    List<Sharpie> removableSharpies = new ArrayList<>();
+    for (Sharpie sharpie : this.getSharpieList()) {
+      if (sharpie.getInkAmount() == 0.0f) {
+        removableSharpies.add(sharpie);
+      }
+    }
+
+    for (Sharpie sharpie : removableSharpies) {
+      this.getSharpieList().remove(sharpie);
+    }
+  }
 }
