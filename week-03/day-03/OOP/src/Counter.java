6a9f0bab75b282269d722702fdd56e1a8ff303b3
hulfyback <hulfyback@gmail.com>
2019-04-17 12:49:06 +0200

    feat(OOP-project): create counter

--- week-03/day-03/OOP/src/Counter.java
+++ week-03/day-03/OOP/src/Counter.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Counter {
+  private int counter;
+
+  public Counter() {
+    this.counter =  0;
+  }
+
+  public Counter(int counter) {
+    this.counter = counter;
+  }
+
+  public int get() {
+    return counter;
+  }
+
+  public void setCounter(int counter) {
+    this.counter = counter;
+  }
+
+  public void add() {
+    this.counter++;
+  }
+
+  public void add(int n) {
+    this.counter += n;
+  }
+
+  public void reset() {
+    this.counter = 0;
+  }
 }
