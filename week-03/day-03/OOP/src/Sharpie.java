9a0f8e2c25e482ce595e98ce04394316d605fd27
hulfyback <hulfyback@gmail.com>
2019-04-17 16:46:12 +0200

    refactor(OOP-project): overload Sharpie constructor

--- week-03/day-03/OOP/src/Sharpie.java
+++ week-03/day-03/OOP/src/Sharpie.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class Sharpie {
+  private String color;
+  private double width;
+  private float inkAmount;
+
+  public Sharpie(String color, double widtht) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  public Sharpie(int inkAmount) {
+    this.color = "bule";
+    this.width = 11;
+    this.inkAmount = inkAmount;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public double getInkAmount() {
+    return this.inkAmount;
+  }
+
+  public void setInkAmount(float inkAmount) {
+    this.inkAmount = inkAmount;
+  }
+
+  public void use() {
+    this.inkAmount -= 1 + (Math.random() * 8);
+  }
 }
