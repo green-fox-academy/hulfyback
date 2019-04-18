36f39d33f727f3206203c3de155483c0e7a4695e
hulfyback <hulfyback@gmail.com>
2019-04-18 14:25:01 +0200

    feat(OOP-project): add 5 maps containing trees

--- week-03/day-03/OOP/src/Trees.java
+++ week-03/day-03/OOP/src/Trees.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Map;
 
 public class Trees {
+  private static Map<String, String> thujaOccidentalis = Map.of(
+      "Type", "Thuja occidentalis",
+      "Leaf Color", "yellowish green",
+      "Age", "60",
+      "Sex", "Male");
+
+  private static Map<String, String> flaximusNigra = Map.of(
+      "Type", "Flaximus Nigra",
+      "Leaf Color", "Green",
+      "Age", "23",
+      "Sex", "Female");
+
+  private static Map<String, String> fagusGandifolia = Map.of(
+      "Type", "Fagus grandifolia",
+      "Leaf Color", "Green",
+      "Age", "33",
+      "Sex", "Male");
+
+  private static Map<String, String> prunusPensylvanica = Map.of(
+      "Type", "Prunus pensylvanica",
+      "Leaf Color", "Green",
+      "Age", "12",
+      "Sex", "Female");
+
+  private static Map<String, String> prunusSerotina = Map.of(
+      "Type", "Prunus serotina",
+      "Leaf Color", "Green",
+      "Age", "9 ",
+      "Sex", "Female");
 }
