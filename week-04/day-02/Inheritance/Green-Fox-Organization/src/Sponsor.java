62f5fe98766d67b6f9b00bdca2cb0d8164b2b321
hulfyback <hulfyback@gmail.com>
2019-04-23 12:19:52 +0200

    feat(green-fox-organization): add new Sponsor subclass

---
+++ week-04/day-02/Inheritance/Green-Fox-Organization/src/Sponsor.java
@@ -0,0 +1,40 @@
+public class Sponsor extends Person {
+  private String company;
+  private int hiredStudents;
+
+  public String getCompany() {
+    return company;
+  }
+
+  public int getHiredStudents() {
+    return hiredStudents;
+  }
+
+  public Sponsor() {
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.hiredStudents = 0;
+    this.company = company;
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + getName() + ", a"
+        + getAge() + " year old + "
+        + getGender() + " who represents "
+        + getCompany() + " and hired " + getHiredStudents() );
+  }
+
+  public void hire() {
+    hiredStudents++;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+}
