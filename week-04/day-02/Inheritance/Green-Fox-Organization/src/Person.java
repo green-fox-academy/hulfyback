a415dbfabda93b3063162e4c5c13bed3596c70a6
hulfyback <hulfyback@gmail.com>
2019-04-23 11:34:37 +0200

    feat(green-fox-organization): add getters

--- week-04/day-02/Inheritance/Green-Fox-Organization/src/Person.java
+++ week-04/day-02/Inheritance/Green-Fox-Organization/src/Person.java
@@ -3,8 +3,32 @@ public class Person {
   private int age;
   private String gender;
 
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
   public void introduce() {
-    System.out.println("Hi, I'm " + name + " a " + age + " year old " + gender);
+    System.out.println("Hi, I'm " + getName() + " a " + getAge() + " year old " + getGender());
   }
   public void getGoal() {
     System.out.println("My goal is: Live for the moment!");
