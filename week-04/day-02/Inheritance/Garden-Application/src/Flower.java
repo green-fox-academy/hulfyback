d4eb6881e8dd52f68a69ba942a091529fe4e5f1d
hulfyback <hulfyback@gmail.com>
2019-04-23 15:49:47 +0200

    refactor(garden-application): remove empty lines and unnecessary parentheses

--- week-04/day-02/Inheritance/Garden-Application/src/Flower.java
+++ week-04/day-02/Inheritance/Garden-Application/src/Flower.java
@@ -4,11 +4,16 @@ public class Flower extends Plant {
   }
 
   @Override
-  public boolean needWater() {
+  public boolean isThirsty() {
     if (getWaterLevel() < 5) {
       return true;
     } else {
       return false;
     }
+  }
+
+  @Override
+  public void absorbWater(double water) {
+    this.setWaterLevel(this.getWaterLevel() + water * 0.75);
   }
 }
