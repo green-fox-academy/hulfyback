6989a56d9413b226b3b43a3e035c425253919e01
hulfyback <hulfyback@gmail.com>
2019-04-23 14:58:48 +0200

    feat(garden-application): add water to the plants

--- week-04/day-02/Inheritance/Garden-Application/src/Garden.java
+++ week-04/day-02/Inheritance/Garden-Application/src/Garden.java
@@ -17,4 +17,58 @@ public class Garden {
   public void addTree(Tree tree) {
     this.trees.add(tree);
   }
+
+  public String printIsThirsty(Plant plant) {
+    if (plant.isThirsty()) {
+      return "The " + plant.toString() + " needs water.";
+    } else {
+      return "The " + plant.toString() + " doesn't need water.";
+    }
+  }
+
+  private int getNumberOfThirstyFlowers() {
+    int numberOfThirstyFlowers = 0;
+    for (Flower flower: flowers) {
+      if (flower.isThirsty()) {
+        numberOfThirstyFlowers++;
+      }
+    }
+    return numberOfThirstyFlowers;
+  }
+
+  private int getNumberOfThirstyTrees() {
+    int numberOfThirstyTrees = 0;
+    for (Tree tree: trees) {
+      if (tree.isThirsty()) {
+        numberOfThirstyTrees++;
+      }
+    }
+    return numberOfThirstyTrees;
+  }
+
+  private int getNumberOfThirstyPlants() {
+    return getNumberOfThirstyFlowers() + getNumberOfThirstyTrees();
+  }
+
+  private void addWaterToFlowers(double water) {
+    for (Flower flower: flowers) {
+      if (flower.isThirsty()) {
+        flower.absorbWater(water / getNumberOfThirstyPlants());
+      }
+    }
+  }
+
+  private void addWaterToTrees(double water) {
+    for (Tree tree: trees) {
+      if (tree.isThirsty()) {
+        tree.absorbWater(water / getNumberOfThirstyPlants());
+      }
+    }
+  }
+
+  public void addWater(double water) {
+    System.out.println("Watering with " + water);
+    addWaterToFlowers(water);
+    addWaterToTrees(water);
+  }
 }
