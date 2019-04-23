02aca0fea087759e6ecd2ca6981fd1112fe8a445
hulfyback <hulfyback@gmail.com>
2019-04-23 17:07:04 +0200

    feat(aircraft-carrier): add getters and setters

--- week-04/day-02/Inheritance/Aircraft-Carrier/src/Carrier.java
+++ week-04/day-02/Inheritance/Aircraft-Carrier/src/Carrier.java
@@ -6,9 +6,35 @@ public class Carrier {
   private int storeOfAmmo;
   private double healthPoint;
 
+  public List<Aircraft> getAircrafts() {
+    return aircrafts;
+  }
+
+  public int getStoreOfAmmo() {
+    return storeOfAmmo;
+  }
+
+  public void setStoreOfAmmo(int storeOfAmmo) {
+    this.storeOfAmmo = storeOfAmmo;
+  }
+
+  public double getHealthPoint() {
+    return healthPoint;
+  }
+
+  public void setHealthPoint(double healthPoint) {
+    this.healthPoint = healthPoint;
+  }
+
   public Carrier(int storeOfAmmo, double healthPoint) {
     this.storeOfAmmo = storeOfAmmo;
     this.healthPoint = healthPoint;
     this.aircrafts = new ArrayList<>();
   }
+
+  public void addAircraft (Aircraft aircraft) {
+    aircrafts.add(aircraft);
+  }
+
+
 }
