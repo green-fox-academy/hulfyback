68491492ceb8c779f283ac11575c4993940e708b
hulfyback <hulfyback@gmail.com>
2019-04-23 17:21:26 +0200

    feat(aircraft-carrier): get status

--- week-04/day-02/Inheritance/Aircraft-Carrier/src/Carrier.java
+++ week-04/day-02/Inheritance/Aircraft-Carrier/src/Carrier.java
@@ -5,6 +5,7 @@ public class Carrier {
   private List<Aircraft> aircrafts;
   private int storeOfAmmo;
   private double healthPoint;
+  private int totalDamage;
 
   public List<Aircraft> getAircrafts() {
     return aircrafts;
@@ -26,10 +27,21 @@ public class Carrier {
     this.healthPoint = healthPoint;
   }
 
+  public int getTotalDamage() {
+    return totalDamage;
+  }
+
+  public void setTotalDamage(int totalDamage) {
+    for (Aircraft aircraft: aircrafts) {
+      this.totalDamage += aircraft.getAllDamage();
+    }
+  }
+
   public Carrier(int storeOfAmmo, double healthPoint) {
     this.storeOfAmmo = storeOfAmmo;
     this.healthPoint = healthPoint;
     this.aircrafts = new ArrayList<>();
+    this.totalDamage = 0;
   }
 
   public void addAircraft (Aircraft aircraft) {
@@ -42,4 +54,15 @@ public class Carrier {
     }
   }
 
+  public String getStatus() {
+    String status =  "HP: " + getHealthPoint()
+        + ", Aircraft count: " + aircrafts.size()
+        + ", Ammo Storage: " + getStoreOfAmmo()
+        + ", Total Damage: " + getTotalDamage() + "\nAircrafts:\n";
+
+    for (Aircraft aircraft: aircrafts) {
+      status += aircraft.getStatus() + "\n";
+    }
+    return status;
+  }
 }
