86a16f633cac97918c5f953d9ca6a5ffa8631cd5
hulfyback <hulfyback@gmail.com>
2019-04-24 14:39:44 +0200

    feat(abstract-instruments): add BassGuitar class, constructor, implement sound() method

---
+++ week-04/day-03/Abstract/Instruments/src/main/java/music/BassGuitar.java
@@ -0,0 +1,13 @@
+package main.java.music;
+
+public class BassGuitar extends StringedInstrument {
+  public BassGuitar() {
+    this.name = "Bass Guitar";
+    this.numberOfStrings = 4;
+  }
+
+  @Override
+  String sound() {
+    return "Duum-duum-duum";
+  }
+}
