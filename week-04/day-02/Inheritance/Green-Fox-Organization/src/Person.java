f63d9e8a8a20e9a5b18bf7ce2dfb27b9193480d1
hulfyback <hulfyback@gmail.com>
2019-04-24 11:11:57 +0200

    feat(green-fox-organization): add setters to the super class Person

--- week-04/day-02/Inheritance/Green-Fox-Organization/src/Person.java
+++ week-04/day-02/Inheritance/Green-Fox-Organization/src/Person.java
@@ -19,12 +19,24 @@ public class Person {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public int getAge() {
     return age;
   }
 
+  public void setAge(int age) {
+    this.age = age;
+  }
+
   public String getGender() {
     return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
   }
 
   public void introduce() {
