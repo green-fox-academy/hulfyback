14847d34c583f059330d0ea37b14390ab8c30e2a
hulfyback <hulfyback@gmail.com>
2019-04-24 12:09:27 +0200

    feat(oop-dominoes): restore reorderDominoes method

--- week-03/day-03/OOP-Dominos/src/Dominoes.java
+++ week-03/day-03/OOP-Dominos/src/Dominoes.java
@@ -15,4 +15,28 @@ public class Dominoes {
   public void addDomino(Domino domino) {
     dominoList.add(domino);
   }
+
+  public List<Domino> reorderDominos(List<Domino> dominos) {
+    List<Domino> orderedDominos = new ArrayList<>();
+
+    orderedDominos.add(dominos.get(0));
+    dominos.remove(0);
+
+    while (true) {
+      int i = 0;
+
+      while (true) {
+        if (orderedDominos.get(orderedDominos.size() - 1).getRightSide() == dominos.get(i).getLeftSide()) {
+          orderedDominos.add(dominos.get(i));
+          dominos.remove(i);
+          break;
+        }
+        i++;
+      }
+      if (dominos.isEmpty()) {
+        break;
+      }
+    }
+    return orderedDominos;
+  }
 }
