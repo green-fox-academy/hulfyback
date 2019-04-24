bc87619f6f4815fd3a6c7c621dc9b4d8233287bb
hulfyback <hulfyback@gmail.com>
2019-04-24 11:59:57 +0200

    refactor(oop-dominoes): add getter, constructor and addDomino() method, remove everything else

--- week-03/day-03/OOP-Dominos/src/Dominoes.java
+++ week-03/day-03/OOP-Dominos/src/Dominoes.java
@@ -2,49 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dominoes {
-  public static void main(String[] args) {
-    List<Domino> dominoes = initializeDominoes();
-    // You have the list of Dominoes
-    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
-    // eg: [2, 4], [4, 3], [3, 5] ...
+  private List<Domino> dominoList;
 
-
-    System.out.println(dominoes);
-
-    System.out.println(reorderDominos(dominoes));
+  public List<Domino> getDominoList() {
+    return dominoList;
   }
 
-  static List<Domino> initializeDominoes() {
-    List<Domino> dominoes = new ArrayList<>();
-    dominoes.add(new Domino(5, 2));
-    dominoes.add(new Domino(4, 6));
-    dominoes.add(new Domino(1, 5));
-    dominoes.add(new Domino(6, 7));
-    dominoes.add(new Domino(2, 4));
-    dominoes.add(new Domino(7, 1));
-    return dominoes;
+  public Dominoes() {
+    this.dominoList = new ArrayList<>();
   }
-  static List<Domino> reorderDominos(List<Domino> dominos) {
-    List<Domino> orderedDominos = new ArrayList<>();
 
-    orderedDominos.add(dominos.get(0));
-    dominos.remove(0);
-
-    while (true) {
-      int i = 0;
-
-      while(true) {
-        if (orderedDominos.get(orderedDominos.size() - 1).getRightSide() == dominos.get(i).getLeftSide()) {
-          orderedDominos.add(dominos.get(i));
-          dominos.remove(i);
-          break;
-        }
-        i++;
-      }
-      if(dominos.isEmpty()) {
-        break;
-      }
-    }
-    return orderedDominos;
+  public void addDomino(Domino domino) {
+    dominoList.add(domino);
   }
 }
