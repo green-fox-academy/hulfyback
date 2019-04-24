693ee0baaeb302e560bb55a2de3ccae00b43f1ea
hulfyback <hulfyback@gmail.com>
2019-04-24 14:54:54 +0200

    fix(abstract-instruments): add name parameter to default constructor of BassGuitar class

--- week-04/day-03/Abstract/Instruments/src/main/java/music/BassGuitar.java
+++ week-04/day-03/Abstract/Instruments/src/main/java/music/BassGuitar.java
@@ -1,9 +1,13 @@
 package main.java.music;
 
 public class BassGuitar extends StringedInstrument {
-  private String name = "Bass Guitar";
+  public BassGuitar() {
+    this.name = "Bass Guitar";
+    this.numberOfStrings = 4;
+  }
 
   public BassGuitar(int numberOfStrings) {
+    this.name = "Bass Guitar";
     this.numberOfStrings = numberOfStrings;
   }
 
