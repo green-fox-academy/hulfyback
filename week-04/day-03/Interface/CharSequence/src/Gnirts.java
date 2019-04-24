71b569cb39bfa60f838a31847d61fe9e774b855a
hulfyback <hulfyback@gmail.com>
2019-04-24 12:51:47 +0200

    feat(interface-charsequence): initialize Gnirts class with getter/setter and constructor

---
+++ week-04/day-03/Interface/CharSequence/src/Gnirts.java
@@ -0,0 +1,15 @@
+public class Gnirts {
+  private String word;
+
+  public String getWord() {
+    return word;
+  }
+
+  public void setWord(String word) {
+    this.word = word;
+  }
+
+  public Gnirts(String word) {
+    this.word = word;
+  }
+}
