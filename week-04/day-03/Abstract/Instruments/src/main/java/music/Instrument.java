e1859b1b0939b38150c9493fcb567abba0d38403
hulfyback <hulfyback@gmail.com>
2019-04-24 14:31:46 +0200

    feat(abstract-instruments): add getter

--- week-04/day-03/Abstract/Instruments/src/main/java/music/Instrument.java
+++ week-04/day-03/Abstract/Instruments/src/main/java/music/Instrument.java
@@ -1,5 +1,12 @@
+package main.java.music;
+
 public abstract class Instrument {
   protected String name;
 
+  public String getName() {
+    return name;
+  }
+
   abstract void play();
+
 }
--- week-04/day-03/Abstract/Instruments/src/main/java/music/StringedInstrument.java
+++ week-04/day-03/Abstract/Instruments/src/main/java/music/StringedInstrument.java
@@ -1,5 +1,6 @@
 abstract public class StringedInstrument extends Instrument {
   protected int numberOfStrings;
-  
+
   abstract void sound();
+  abstract void play();
 }
