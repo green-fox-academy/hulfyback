7b0386119d1e34aa185f5cf9044b6c731625d199
hulfyback <hulfyback@gmail.com>
2019-04-24 12:34:02 +0200

    feat(week03-day03-todo): implement Comparable interface and compareTo() method

--- week-03/day-03/Fleet/src/Thing.java
+++ week-03/day-03/Fleet/src/Thing.java
@@ -1,9 +1,25 @@
 /**
  * Created by aze on 2017.03.29..
  */
-public class Thing {
+public class Thing implements Comparable<Thing> {
   private String name;
   private boolean completed;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
 
   public Thing(String name) {
     this.name = name;
@@ -16,5 +32,14 @@ public class Thing {
   @Override
   public String toString() {
     return (completed ? "[x] " : "[ ] ") + name;
+  }
+
+  @Override
+  public int compareTo(Thing thing) {
+    int result = Boolean.compare(this.isCompleted(), thing.isCompleted());
+    if (result == 0) {
+      result = this.getName().compareTo(thing.getName());
+    }
+    return result;
   }
 }
