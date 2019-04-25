f8940e4ac25c1c580541a8420571e5426233485e
hulfyback <hulfyback@gmail.com>
2019-04-25 16:17:33 +0200

    feat(Testing): import Animal class

---
+++ week-04/day-04/Testing/src/main/java/exercises/Animal.java
@@ -0,0 +1,49 @@
+package main.java.exercises;
+
+public class Animal {
+  private int hungry;
+  private int thirst;
+
+  public Animal() {
+    this.hungry = 50;
+    this.thirst = 50;
+  }
+
+  public Animal(int hungry) {
+    this.hungry = hungry;
+  }
+
+  public int getHungry() {
+    return hungry;
+  }
+
+  public void setHungry(int hungry) {
+    this.hungry = hungry;
+  }
+
+  public int getThirst() {
+    return thirst;
+  }
+
+  public void setThirst(int thirst) {
+    this.thirst = thirst;
+  }
+
+  public void eat() {
+    this.hungry--;
+  }
+
+  public void drink() {
+    this.thirst--;
+  }
+
+  public void play() {
+    this.hungry++;
+    this.thirst++;
+  }
+
+  @Override
+  public String toString() {
+    return Integer.toString(this.getHungry());
+  }
+}
