21e9cacff8e87bf7db4357420462aa7bbb386c2c
hulfyback <hulfyback@gmail.com>
2019-04-25 16:02:30 +0200

    feat(Testing): import Sharpie class

---
+++ week-04/day-04/Testing/src/main/java/exercises/Sharpie.java
@@ -0,0 +1,47 @@
+package main.java.exercises;
+
+public class Sharpie {
+  private String color;
+  private double width;
+  private float inkAmount;
+
+  public Sharpie(String color, double width) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  public Sharpie(int inkAmount) {
+    this.color = "blue";
+    this.width = 11;
+    this.inkAmount = inkAmount;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
+
+  public double getInkAmount() {
+    return this.inkAmount;
+  }
+
+  public void setInkAmount(float inkAmount) {
+    this.inkAmount = inkAmount;
+  }
+
+  public void use() {
+    this.inkAmount -= 1 + (Math.random() * 8);
+  }
+}
