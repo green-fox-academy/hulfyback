b29446208e1e8f2d7a5403d133e6946c2df5583a
hulfyback <hulfyback@gmail.com>
2019-04-25 16:35:45 +0200

    test(Testing): test setThirsty(60) method on Animal class

--- week-04/day-04/Testing/src/main/java/test/AnimalTest.java
+++ week-04/day-04/Testing/src/main/java/test/AnimalTest.java
@@ -1,25 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
 import main.java.exercises.Animal;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class AnimalTest {
-  Animal animal;
+
+  private Animal animal;
 
   @Before
-
   public void setup() {
     animal = new Animal();
   }
 
   @Test
-
   public void getHungryShouldReturn50() {
     assertEquals(50, animal.getHungry());
   }
 
   @Test
-
   public void setHungryShouldSetHungry60() {
     animal.setHungry(60);
 
@@ -27,8 +26,14 @@ public class AnimalTest {
   }
 
   @Test
-
   public void getThirstyShouldReturn50OnDefaultContructor() {
     assertEquals(50, animal.getThirsty());
+  }
+
+  @Test
+  public void setThirstyShouldSetThirsty60() {
+    animal.setThirsty(60);
+    
+    assertEquals(60, animal.getThirsty());
   }
 }
