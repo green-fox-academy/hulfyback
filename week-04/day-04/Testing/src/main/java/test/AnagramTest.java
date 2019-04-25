cb4ba21ce04c08cd8d51394473f4f4cfe627ac07
hulfyback <hulfyback@gmail.com>
2019-04-25 13:06:26 +0200

    test(Testing): test isAnagram working correctly

---
+++ week-04/day-04/Testing/src/main/java/test/AnagramTest.java
@@ -0,0 +1,21 @@
+import main.java.exercises.Anagram;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnagramTest {
+
+  Anagram anagram;
+
+  @Before
+
+  public void setup() {
+    anagram = new Anagram();
+  }
+
+  @Test
+
+  public void isAnagramShouldReturnTrue() {
+    Assert.assertEquals(true, anagram.isAnagram("sonka", "kanos"));
+  }
+}
