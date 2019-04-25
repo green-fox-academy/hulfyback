064cc83bedb5fc83d10c7ae22865343e10d38c06
hulfyback <hulfyback@gmail.com>
2019-04-25 11:33:24 +0200

    feat(testing): add Apple class

---
+++ week-04/day-04/Testing/src/main/java/exercises/Apple.java
@@ -0,0 +1,13 @@
+package main.java.exercises;
+
+public class Apple {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public Apple() {
+    this.name = "apple";
+  }
+}
