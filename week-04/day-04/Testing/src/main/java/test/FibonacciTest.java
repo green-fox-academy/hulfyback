7c57bbe95c5a311e8d42933bb7d3dad6f82991f7
hulfyback <hulfyback@gmail.com>
2019-04-25 15:30:01 +0200

    test(Testing): test fibonacci on 0 and 3 and 6

--- week-04/day-04/Testing/src/main/java/test/FibonacciTest.java
+++ week-04/day-04/Testing/src/main/java/test/FibonacciTest.java
@@ -15,13 +15,25 @@ public class FibonacciTest {
 
   @Test
 
-  public void getFibonacciNumberReturnZeroOnZero() {
+  public void getFibonacciNumberShouldReturnZeroOnZero() {
     Assert.assertEquals(0, fibonacci.getFibonacciNumber(0));
   }
 
   @Test
 
-  public void getFibonacciNumberReturnOneOnOne() {
+  public void getFibonacciNumberShouldReturnOneOnOne() {
     Assert.assertEquals(1, fibonacci.getFibonacciNumber(1));
+  }
+
+  @Test
+
+  public void getFibonacciNumberShouldReturnTwoOnThre() {
+    Assert.assertEquals(2, fibonacci.getFibonacciNumber(3));
+  }
+
+  @Test
+
+  public void getFibonacciNumberShouldReturnEightOnSix() {
+    Assert.assertEquals(8, fibonacci.getFibonacciNumber(6));
   }
 }
