1a55e380f392e93ed6d21de9d08f717d7db793d9
hulfyback <hulfyback@gmail.com>
2019-04-25 15:08:11 +0200

    test(testing): test countLetters on 'ABA'

--- week-04/day-04/Testing/src/main/java/test/CountLettersTest.java
+++ week-04/day-04/Testing/src/main/java/test/CountLettersTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 
 public class CountLettersTest {
   private CountLetters countLetters;
+  private HashMap<Character, Integer> lettersHashMap;
 
   @Before
 
@@ -19,4 +20,19 @@ public class CountLettersTest {
   public void countLettersShouldReturnA2OnAA() {
     Assert.assertEquals(new HashMap<Character, Integer>() {{put('A', 2);}}, countLetters.countLetters("AA"));
   }
+
+  @Test
+
+  public void countLettersShouldReturnA2B1OnABA() {
+    lettersHashMap = new HashMap<Character, Integer>() {
+      {
+        put('A', 2);
+        put('B', 1);
+      }
+    };
+    
+    Assert.assertEquals(lettersHashMap, countLetters.countLetters("ABA"));
+  }
+
+
 }
