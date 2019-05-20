a5df3a73f57b3ac566223afcd28797c81fbf4029
hulfyback <hulfyback@gmail.com>
2019-05-20 14:52:03 +0200

    feat(bacnkaccount): mark animals are kings

--- week-07/day-01/bank_of_simba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountsController.java
+++ week-07/day-01/bank_of_simba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankAccountsController.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BankAccountsController {
 
   private List<BankAccount> bankAccountList = Arrays.asList(
-      new BankAccount("Simba", 2000, "lion"),
-      new BankAccount("Timon", 125, "surikata"),
-      new BankAccount("Pumbaa", 88, "boar"),
-      new BankAccount("Zazu", 1125, "hornbill"),
-      new BankAccount("Scar", 587.25, "lion")
+      new BankAccount("Simba", 2000, "lion", true),
+      new BankAccount("Timon", 125, "surikata", false),
+      new BankAccount("Pumbaa", 88, "boar", false),
+      new BankAccount("Zazu", 1125, "hornbill", false),
+      new BankAccount("Scar", 587.25, "lion", false)
   );
 
   @RequestMapping("/start")
--- week-07/day-01/bank_of_simba/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
+++ week-07/day-01/bank_of_simba/src/main/java/com/greenfoxacademy/bankofsimba/models/BankAccount.java
@@ -6,6 +6,7 @@ public class BankAccount {
   private String name;
   private double balance;
   private String animalType;
+  private boolean isKing;
 
   public String formatBankAccount(double account) {
     DecimalFormat df = new DecimalFormat("#,###.##");
@@ -16,10 +17,11 @@ public class BankAccount {
   public BankAccount() {
   }
 
-  public BankAccount(String name, double balance, String animalType) {
+  public BankAccount(String name, double balance, String animalType, boolean isKing) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+    this.isKing = isKing;
   }
 
   public String getName() {
@@ -44,5 +46,13 @@ public class BankAccount {
 
   public void setAnimalType(String animalType) {
     this.animalType = animalType;
+  }
+
+  public boolean isKing() {
+    return isKing;
+  }
+
+  public void setKing(boolean king) {
+    isKing = king;
   }
 }
