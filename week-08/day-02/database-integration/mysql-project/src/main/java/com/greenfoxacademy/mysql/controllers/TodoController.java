4381a5afb1e6fa288b4a40be7723aa10cfcd9870
Zsolt Molnar <hulfyback@gmail.com>
2019-05-27 23:45:59 +0200

    feat(mysql-todo-w8d2):list undone and done tasks

--- week-08/day-02/database-integration/mysql-project/src/main/java/com/greenfoxacademy/mysql/controllers/TodoController.java
+++ week-08/day-02/database-integration/mysql-project/src/main/java/com/greenfoxacademy/mysql/controllers/TodoController.java
@@ -1,10 +1,14 @@
 package com.greenfoxacademy.mysql.controllers;
 
+import com.greenfoxacademy.mysql.models.Todo;
 import com.greenfoxacademy.mysql.repositories.TodoRepository;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -16,6 +20,14 @@ public class TodoController {
   public String list(Model model) {
 //    todoRepository.save(new Todo("Start the day", true, false));
     model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
+  }
+
+  @RequestMapping("/todo/")
+  public String listActiveActions(Model model, @RequestParam boolean isActive) {
+    Stream<Todo> todoStream = StreamSupport.stream(todoRepository.findAll().spliterator(), false)
+        .filter(todo -> todo.isDone() == isActive);
+    model.addAttribute("todos", (Iterable<Todo>) todoStream::iterator);
     return "todolist";
   }
 }
