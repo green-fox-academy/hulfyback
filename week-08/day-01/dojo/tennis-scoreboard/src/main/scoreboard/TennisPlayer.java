eb0acfe793445b5f45ad06af98cd37ee35d06f0c
Zsolt Molnar <hulfyback@gmail.com>
2019-05-27 10:51:26 +0200

    feat(dojo-tennis-scoreboard-w08d01): add TennisPlayer class

---
+++ week-08/day-01/dojo/tennis-scoreboard/src/main/scoreboard/TennisPlayer.java
@@ -0,0 +1,26 @@
+package main.scoreboard;
+
+public class TennisPlayer {
+  private int score;
+  private String name;
+
+  public TennisPlayer(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
